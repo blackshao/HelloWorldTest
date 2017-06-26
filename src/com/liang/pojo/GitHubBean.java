59eb667f10a8a8bb0b87be143897bf6fe27611da
Liangwj <my@137.200.34.149>
2017-06-26 16:30:35 +0800

    final test

---
+++ src/com/liang/pojo/GitHubBean.java
@@ -0,0 +1,44 @@
+package com.liang.pojo;
+
+public class GitHubBean {
+	
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String age;
+	
+	private String userName;
+	
+	private String password;
+}
